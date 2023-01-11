d0c579f5bc712ba7c7535e018d49e42496719df6
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2023-01-11 16:53:30 +0300

    last commit - dropmenu transition özelliği iyileştirildi.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ var hamburgerButton = document.querySelector('.dropLi');
 var dropdownMenu = document.querySelector('.dropMenu');
 
 hamburgerButton.addEventListener('click', function () {
-    if (dropdownMenu.style.display === 'none') {
-        dropdownMenu.style.display = 'block';
+    if (dropdownMenu.style.opacity == 0) {
+        dropdownMenu.style.opacity = 1;
     } else {
-        dropdownMenu.style.display = 'none';
+        dropdownMenu.style.opacity = 0;
     }
 });
